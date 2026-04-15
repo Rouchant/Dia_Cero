2adf46a30ae4f00e50b8223cbd8434acd20e98f3
Rouchant <jmemarchant@gmail.com>
2026-04-15 19:22:54 -0400

    agranda logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function Logo({ className = "h-8" }: { className?: string }) {
+export function Logo({ className = "h-12 w-auto" }: { className?: string }) {
   return (
     <img
       src="/logo.svg"
