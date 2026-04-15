bd393ffbfea540c688f0c064dd8a7c6714222611
Rouchant <jmemarchant@gmail.com>
2026-04-15 19:21:54 -0400

    cambio a logo.svg

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Logo({ className = "h-8" }: { className?: string }) {
   return (
     <img
-      src="/logo.png"
+      src="/logo.svg"
       alt="Dia Cero Logo"
       className={`object-contain mix-blend-normal ${className}`}
     />
